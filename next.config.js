0ba933491cb77bdb08867ef52e9c5e87702396ad
zlexif <ahmad23slieman@gmail.com>
2025-02-02 19:12:47 +0200

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
   images: {
     unoptimized: true,
-    domains: ["v0.blob.com"],
   },
-  // Since we're deploying to auradevelopment5m.github.io directly
-  // we don't need basePath or assetPrefix
+  assetPrefix: '.',
   trailingSlash: true,
 }
 
 module.exports = nextConfig
-
