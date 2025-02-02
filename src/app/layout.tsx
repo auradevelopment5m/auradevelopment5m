efa6009078f931cf2cfc07320e9dd32b2aa6686a
zlexif <ahmad23slieman@gmail.com>
2025-02-02 19:41:49 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./styles/globals.css"
+import "@/styles/globals.css"
 import { Inter } from "next/font/google"
 import Header from "./components/header"
 import type React from "react"
@@ -6,7 +6,6 @@ import type React from "react"
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
-  adjustFontFallback: false,
 })
 
 export default function RootLayout({
@@ -16,9 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <link rel="stylesheet" href="./styles/globals.css" />
-      </head>
       <body className={`${inter.className} bg-black text-white`}>
         <Header />
         {children}
